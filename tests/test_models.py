57a1cf1e73e65c378b24d058223601a7f4c51e86
yulkes <yulkes@gmail.com>
2020-07-19 17:32:16 +0300

    Testing for DirectoryListing model separation behavior

---
+++ tests/test_models.py
@@ -0,0 +1,20 @@
+import tempfile
+from pathlib import Path
+
+from models import DirectoryListing
+
+
+def test_directory_listing_should_detect_files():
+    listing = DirectoryListing(Path())
+    with tempfile.NamedTemporaryFile() as tf:
+        listing.add_file(Path(tf.name))
+        assert listing.files
+        assert not listing.dirs
+
+
+def test_directory_listing_should_detect_directories():
+    listing = DirectoryListing(Path())
+    with tempfile.TemporaryDirectory() as tf:
+        listing.add_file(Path(tf))
+        assert listing.dirs
+        assert not listing.files
