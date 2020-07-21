47994b8cdc69a1486178b23c4619d1798de0c8a1
yulkes <yulkes@gmail.com>
2020-07-21 17:18:37 +0300

    Implement FileSystem delete functionality

--- ifs/fs.py
+++ ifs/fs.py
@@ -1,4 +1,5 @@
 import abc
+import os
 from abc import abstractmethod
 from pathlib import Path
 
@@ -30,36 +31,45 @@ class RealFileSystemService(AbstractFileSystemService):
 
     def get_dir_list(self, request: DirListingRequest) -> DirectoryListing:
         dir_list_path = request.base_path
-        full_path = self.validate_safe_path(dir_list_path)
-
+        full_path = self._validate_safe_path(dir_list_path)
         result = DirectoryListing(dir_list_path)
         for file in full_path.iterdir():
             result.add_file(file)
         return result
 
-    def validate_safe_path(self, path):
+    def _validate_safe_path(self, path, force_directory=True):
         if path.is_absolute():
             raise ValueError(
                 f"Security Error, absolute paths are not allowed: {path}", path
             )
         full_path = (self.root_path / path).resolve()
         # Try to catch tricks to go outside the scope of the given root path (Like using ".." for dir traversal)
-        if full_path > self.root_path:
+        if self.root_path not in full_path.parents and self.root_path != full_path:
             raise ValueError(
                 f"Security Error, file path is outside allowed root: {path}", path
             )
         if not full_path.exists():
-            raise ValueError(
-                f"Given path does not exist: {full_path.name}", full_path.name
-            )
-        if not full_path.is_dir():
-            raise ValueError(
-                f"Given path is not a directory: {full_path.name}", full_path.name
-            )
+            raise ValueError(f"Given path does not exist: {path}", path)
+        if force_directory and not full_path.is_dir():
+            raise ValueError(f"Given path is not a directory: {path}", path)
         return full_path
 
-    def rename_file(self, request: RenameFileRequest) -> bool:
+    def rename_file(self, request: RenameFileRequest):
         return False
 
-    def delete_file(self, request: DeleteFileRequest) -> bool:
-        return False
+    def delete_file(self, request: DeleteFileRequest):
+        full_path = self._validate_safe_path(request.file_path, force_directory=False)
+        if full_path.is_dir():
+            try:
+                full_path.rmdir()
+            except OSError as oe:
+                raise ValueError(
+                    f"Failed deleting directory: {request.file_path}"
+                ) from oe
+        elif full_path.is_file():
+            os.remove(full_path)
+        else:
+            raise ValueError(
+                f"Cannot delete this type of file: {request.file_path}",
+                request.file_path,
+            )
--- tests/test_fs.py
+++ tests/test_fs.py
@@ -5,6 +5,7 @@ import pytest
 
 from ifs.fs import RealFileSystemService
 from ifs.models import DirListingRequest
+from models import DeleteFileRequest
 
 
 @pytest.fixture()
@@ -18,25 +19,40 @@ def filesystem(temporary_dir):
     yield RealFileSystemService(root_path=Path(temporary_dir))
 
 
-def test_real_file_system_should_return_empty_directory(filesystem):
+def test_real_fs_should_return_empty_directory(filesystem):
     result = filesystem.get_dir_list(DirListingRequest(Path(".")))
     assert not result
 
 
-def test_real_file_system_should_block_absolute_urls(filesystem):
+def test_real_fs_system_should_block_absolute_urls(filesystem):
     with pytest.raises(ValueError):
         filesystem.get_dir_list(DirListingRequest(Path("/test")))
 
 
-def test_real_file_system_should_block_urls_outside_root(filesystem):
+def test_real_fs_should_block_urls_outside_root(filesystem):
     with pytest.raises(ValueError):
         filesystem.get_dir_list(DirListingRequest(Path("../../test")))
 
 
-def test_real_file_system_should_return_files(temporary_dir, filesystem):
+def test_real_fs_should_return_files(temporary_dir, filesystem):
     with tempfile.TemporaryDirectory(
         dir=temporary_dir
     ) as testdir, tempfile.NamedTemporaryFile(dir=temporary_dir) as testfile:
         dirlist = filesystem.get_dir_list(DirListingRequest(Path(".")))
         assert Path(testdir).resolve() in dirlist.dirs
         assert Path(testfile.name).resolve() in dirlist.files
+
+
+def test_real_fs_delete_existing_file_should_succeed(temporary_dir, filesystem):
+    with tempfile.NamedTemporaryFile(dir=temporary_dir, delete=False) as testfile:
+        assert Path(testfile.name).exists()
+        print(f"\n{testfile.name=}\n{temporary_dir=}")
+        filesystem.delete_file(DeleteFileRequest(Path(Path(testfile.name).name)))
+        assert not Path(testfile.name).exists()
+
+
+def test_real_fs_delete_nonexisting_file_should_fail(filesystem):
+    with pytest.raises(ValueError):
+        nonexisting_file = Path("thisfiledoesnotexist")
+        assert not nonexisting_file.exists()
+        filesystem.delete_file(DeleteFileRequest(nonexisting_file))
