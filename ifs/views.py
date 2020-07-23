383fc6fc37bb429e8b4c6c013575d9e07a844f2d
yulkes <yulkes@gmail.com>
2020-07-23 15:50:15 +0300

    Initializing the FileSystem service on Blueprint's registration

--- ifs/views.py
+++ ifs/views.py
@@ -1,11 +1,27 @@
 from pathlib import Path
 
-from flask import Blueprint, current_app, jsonify
+from flask import Blueprint, current_app, jsonify, request
 
 from ifs.fs import RealFileSystemService
 from ifs.models import DirListingRequest
+from models import DeleteFileRequest, RenameFileRequest
 
 fs = Blueprint("fs", "fs")
+
+
+@fs.record
+def set_app_fs_service():
+    base_path_str = current_app.config.get("BASE_PATH", "")
+    if not base_path_str:
+        raise ValueError(
+            "Initialized without BASE_PATH variable for the FileSystem module"
+        )
+    base_path = Path(base_path_str).resolve()
+    if not base_path.exists() or not base_path.is_dir():
+        raise ValueError(
+            f"BASE_PATH [{base_path_str}] should reflect an existing directory"
+        )
+    current_app.fs_service = RealFileSystemService(base_path)
 
 
 @fs.route("/", methods=["GET"])
@@ -24,11 +40,10 @@ def get_listing(listing_path=Path(".")):
     }
     ```
     """
-    current_app.logger.warning("This is the path received: [%s]", listing_path)
+    current_app.logger.info("This is the path received: [%s]", listing_path)
     try:
-        request = DirListingRequest(Path(listing_path))
-        fs_service = RealFileSystemService(Path("."))
-        result = fs_service.get_dir_list(request)
+        dir_list_request = DirListingRequest(Path(listing_path))
+        result = current_app.fs_service.get_dir_list(dir_list_request)
     except ValueError as e:
         return jsonify({"success": False, "error": e.args[0]})
     return jsonify({"success": True, "fs": result.to_dict()})
@@ -36,10 +51,22 @@ def get_listing(listing_path=Path(".")):
 
 @fs.route("/<path:path>", methods=["DELETE"])
 def delete_file(path):
-    pass
+    current_app.logger.info("This is the path received: [%s]", path)
+    try:
+        delete_request = DeleteFileRequest(Path(path))
+        current_app.fs_service.delete_file(delete_request)
+        return jsonify(success=True)
+    except ValueError as e:
+        return jsonify(success=False, error=e.args[0])
 
 
 @fs.route("/<path:path>", methods=["PUT"])
 def rename_file(path):
-    # TODO: Get target path from body
-    pass
+    current_app.logger.info("This is the path received: [%s]", path)
+    try:
+        new_path = Path(request.json.get("name", ""))
+        rename_request = RenameFileRequest(Path(path), Path(new_path))
+        current_app.fs_service.rename_file(rename_request)
+        return jsonify(success=True)
+    except ValueError as e:
+        return jsonify(success=False, error=e.args[0])
